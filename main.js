74f42ad7068934118d53bf661316ae1a07ab251a
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2017-05-25 15:46:38 +0200

    Orientation based on movement - drawing path

--- main.js
+++ main.js
@@ -1,18 +1,62 @@
 
-var g_view = new ol.View({ center: [0, 0], zoom: 14 });
+var g_view = new ol.View({ center: [0, 0], zoom: 17 });
 var g_map = null;
-var g_lastLonLat = [ 0, 0 ];
+var g_lastCenter = null;
+var g_lastLonLat = null;
+var g_earthSphere = new ol.Sphere(6378137);
+var g_lineString = new ol.geom.LineString([]);
+var g_pathFeature = new ol.Feature({ geometry: g_lineString });
 
 function positionCallback(pos)
 {
     //console.log(pos);
     //console.log([pos.coords.longitude, pos.coords.latitude]);
-    g_lastLonLat = [ pos.coords.longitude, pos.coords.latitude ];
 
-	var newCenter = ol.proj.fromLonLat(g_lastLonLat, g_view.getProjection().getCode());
-	console.log("pos.coords = (" + g_lastLonLat[0] + "," + g_lastLonLat[1] + ")");
-    console.log("newCenter = (" + newCenter[0] + "," + newCenter[1] + ")");	
+    var newLonLat = [ pos.coords.longitude, pos.coords.latitude ];
+	var newCenter = ol.proj.fromLonLat(newLonLat, g_view.getProjection().getCode());
     g_view.setCenter(newCenter);
+
+    if (g_lastLonLat != null)
+    {
+        var dist = g_earthSphere.haversineDistance(g_lastLonLat, newLonLat);
+        if (dist > 5.0) // try to avoid GPS coord jumps to modifiy the orientation
+        {
+            var dx = newCenter[0] - g_lastCenter[0];
+            var dy = newCenter[1] - g_lastCenter[1];
+            var angle = 0;
+
+            if (dx*dx < 1e-10) // nearly zero
+            {
+                if (dy > 0)
+                    angle = Math.PI*0.5;
+                else
+                    angle = -Math.PI*0.5;
+            }
+            else
+            {
+                var r = Math.sqrt(dx*dx+dy*dy);
+                var c = dx/r;
+
+                angle = Math.acos(c);
+                if (dy < 0)
+                    angle = 2.0*Math.PI-angle;
+
+                //console.log(angle*180.0/Math.PI);
+            }
+
+            g_view.setRotation(3.0*Math.PI*0.5+angle);
+
+            g_lastLonLat = newLonLat;
+            g_lastCenter = newCenter;
+        }
+    }
+    else
+    {
+        g_lastLonLat = newLonLat;
+        g_lastCenter = newCenter;
+    }
+
+    g_lineString.appendCoordinate(newCenter);
 }
 
 function positionError(err)
@@ -23,7 +67,9 @@ function positionError(err)
 function main()
 {
     g_map = new ol.Map({
-        layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }) ],
+        layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }), 
+                  new ol.layer.Vector({ source: new ol.source.Vector({features: [g_pathFeature] }) })
+        ],
         target: 'map',
         view: g_view,
         renderer: 'webgl',
@@ -40,7 +86,27 @@ function main()
         timeout: 1000,
         maximumAge: 0
     };
-    navigator.geolocation.watchPosition(positionCallback, positionError, options);
+    //navigator.geolocation.watchPosition(positionCallback, positionError, options);
+    
+    var f = function()
+    {
+        var center = [5.4278813, 50.9167316];
+        var radius = 0.01;
+        var t = 0;
+
+        return function()
+        {
+            t += 0.005;
+            return [ center[0]+radius*Math.cos(t), center[1]+radius*Math.sin(t) ];
+        }
+    }
+    var newPosFunction = f();
+    setInterval(function() {
+        var p = newPosFunction();
+        var obj = { coords: { longitude: p[0], latitude: p[1] } };
+        //console.log("Fake pos: " + JSON.stringify(obj));
+        positionCallback(obj);
+    },500);
 
     /*
     setInterval(function() {
